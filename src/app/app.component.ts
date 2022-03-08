8cb7063065ea4fa574a42087733f3b851b7f15f9
Venkata Sanku <bkesav2@gmail.com>
2022-03-07 20:06:24 -0800

    assignment1 updated
    added code in the following files
    html
    ts
    module
    tsconfig.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pa-starter-angular';
-}
+export class AppComponent implements OnInit {
+  title = 'my-dream-app';
+  showForm = false;
+  showlist = false;
+  message = true;
+  data: Array<string> = [];
+  displayList: any;
+  hadleError: string;
+
+  restarentForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.restarentForm = this.fb.group(
+      {
+        item: new FormControl(''),
+        description: new FormControl(''),
+    });
+    }
+    onSubmit() {
+      const info = {
+        item: this.restarentForm.value.item,
+        description: this.restarentForm.value.description,
+      };
+      this.showList(info);
+      this.showmessage();
+      this.hideForm();
+     }
+    
+     showList(info:any) {
+      if (info.item && info.description) {
+      this.data.push(info);
+      this.displayList = this.data;
+      this.showrecipes();
+    } else {
+      this.hadleError = 'recipe-name and recipe instructions are requried';
+    }
+    }
+    showrecipes(){
+      this.showlist = true;
+    }
+    showmessage(){
+      this.message = false;
+    }
+    hideForm(){
+      this.showForm = false;
+    }
+
+    addRecipes(){
+      this.showForm = true;
+      this.showlist = false;
+    }
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
